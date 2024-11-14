b50f2c6b62b252f7e79c34202ee27a4b9a412de3
harshit8741 <harshits0240@gmail.com>
2024-11-14 10:07:06 +0530

    testing netlify

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 export default function AuthPage () {
     return <div className="bg-[#050505] flex flex-col justify-center h-screen">
         <div className="text-white flex flex-row justify-center">
             Auth
+        </div>
+        <div>
+            <NavLink to="/overview" className="bg-white">
+                Overview
+            </NavLink>
         </div>
     </div>
 }
